5d989550d28c21bcc327a5dbec7ddeaeac0941c2
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-07-10 00:16:45 -0400

    done with card display

--- assets/pages/events.js
+++ assets/pages/events.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Text, ScrollView, View, TouchableOpacity } from 'react-native';
 import HeaderComponent from './header';
 import { useNavigation } from '@react-navigation/native';
-import { Card, CardTitle, CardContent, CardText } from 'react-native-material-cards';
+import { Card, CardTitle, CardContent, CardImage, CardAction, CardButton } from 'react-native-material-cards';
 
 
 const { styles } = require('../style');
@@ -14,24 +14,39 @@ export default EventsScreen = () => {
     const navigation = useNavigation();
     const [categories, setCategories] = React.useState([]);
 
-    const data = async () => {
+    React.useEffect( () => {
         let tempCat = [];
         db.collection('event').get().then(snap => {
             snap.forEach(el => {
                 tempCat.push(
-                    <Card>
-                        <CardTitle>{el.data().image}</CardTitle>
-                        <CardText>
-                            {el.data().name}
-                        </CardText>
-                        <CardContent text={el.data().event_date} />
+                    <Card style={{width: '90%'}}>
+                        <CardImage
+                            source={{ uri: el.data().image }}
+                            title="Above all i am here"
+                        />
+                        <CardTitle
+                            title={el.data().name}
+                            subtitle={el.data().event_date.toDate().toString()}
+                        />
+                        <CardContent text={el.data().description} />
+                        <CardAction
+                            separator={true}
+                            inColumn={false}>
+                            <CardButton
+                                // onPress={() => { }}
+                                style = {{width: '100%'}}
+                                title="See"
+                                color="blue"
+                            />
+                            
+                        </CardAction>
                     </Card>
-                    // <Text>{el.data().name} - {el.data().description}</Text>
+                    
                 )
             });
             setCategories(tempCat);
         });
-    }
+    })
 
     return (
         <ScrollView>
@@ -43,7 +58,7 @@ export default EventsScreen = () => {
                         Create Event
                     </Text>
                 </TouchableOpacity>
-                <Text onPress={(data)}>Click</Text>
+                {/* <Text onPress={(data)}>Click</Text> */}
                 <>
                     {categories}
                 </>
