7bb25576134e427da3ac5235e8b8404819e000b9
Nilkumar Patel <nsp4898@gmail.com>
2021-07-27 18:15:19 -0400

    worked on Group description page, now shows associated members and events

--- assets/pages/groupDescription.js
+++ assets/pages/groupDescription.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Text, TouchableOpacity, Pressable, ScrollView, View } from 'react-native';
+import { Text, TouchableOpacity, Pressable, ScrollView, View, SafeAreaView, FlatList } from 'react-native';
 import AuthContext from '../../context';
+import firebase from 'firebase';
 
 const { styles } = require('../style');
 const db = require('../../db_conn');
@@ -9,6 +10,8 @@ export default GroupDescriptionScreen = ({ route, navigation }) => {
     const { state, dispatch } = React.useContext(AuthContext);
     const groupId = route.params.groupId;
     const [member, setMember] = React.useState('');
+    const [groupMembers, setGroupMembers] = React.useState([]);
+    const [groupEvents, setGroupEvents] = React.useState([]);
     const [groupInfo, setGroupInfo] = React.useState({
         description: '',
         image: '',
@@ -18,6 +21,9 @@ export default GroupDescriptionScreen = ({ route, navigation }) => {
     });
 
     React.useEffect(() => {
+        setGroupMembers([]);
+        setGroupEvents([]);
+        // getting group and user information
         db.collection('group')
             .doc(route.params.groupId)
             .get()
@@ -37,6 +43,7 @@ export default GroupDescriptionScreen = ({ route, navigation }) => {
                 }));
             });
 
+        // checking if the user is a member of this group
         db.collection('members')
             .where('userId', '==', state.userToken)
             .where('groupId', '==', groupId)
@@ -45,8 +52,72 @@ export default GroupDescriptionScreen = ({ route, navigation }) => {
                 if (snap.docs.length > 0) {
                     setMember(snap.docs[0].id);
                 }
-            })
-    }, []);
+            });
+
+        // getting all the members of the group
+        db.collection('members')
+            .where('groupId', '==', groupId)
+            .get()
+            .then((snap) => {
+                if (snap.docs.length > 0) {
+                    const userIds = snap.docs.map(el => el.data().userId);
+                    db.collection('user')
+                        .where(firebase.firestore.FieldPath.documentId(), 'in', userIds)
+                        .get()
+                        .then((userData) => {
+                            let members = [];
+                            for (var i in userData.docs) {
+                                const el = userData.docs[i];
+                                members.push({
+                                    id: el.id,
+                                    name: el.data().first_name + ' ' + el.data().last_name,
+                                    email: el.data().email
+                                });
+                            }
+                            setGroupMembers(members);
+                        });
+                } else {
+                    setGroupMembers([]);
+                }
+            });
+
+        // getting all the events associated with this group
+        db.collection('members')
+            .where('userId', '==', state.userToken)
+            .where('groupId', '==', groupId)
+            .get()
+            .then((snap) => {
+                if (snap.docs.length > 0) {
+                    setMember(snap.docs[0].id);
+                }
+            });
+
+        // getting all the members of the group
+        db.collection('event')
+            .where('group', '==', db.collection('group').doc(groupId))
+            .get()
+            .then((snap) => {
+                if (snap.docs.length > 0) {
+                    const eventIds = snap.docs.map(el => el.id);
+                    db.collection('event')
+                        .where(firebase.firestore.FieldPath.documentId(), 'in', eventIds)
+                        .get()
+                        .then((eventData) => {
+                            let events = [];
+                            for (var i in eventData.docs) {
+                                const el = eventData.docs[i];
+                                events.push({
+                                    id: el.id,
+                                    name: el.data().name
+                                });
+                            }
+                            setGroupEvents(events);
+                        });
+                } else {
+                    setGroupEvents([]);
+                }
+            });
+    }, [member]);
 
     const joinGroup = async () => {
         db.collection('members')
@@ -68,28 +139,51 @@ export default GroupDescriptionScreen = ({ route, navigation }) => {
     }
 
     return (
-        <ScrollView keyboardShouldPersistTaps='handled'>
-            <View style={styles.container}>
-                <TouchableOpacity onPress={() => navigation.navigate("Create Event", { groupId: route.params.groupId })}
-                    style={styles.guideButtons}>
-                    <Text style={[styles.invertText, styles.text]}>
-                        Create Event
-                    </Text>
-                </TouchableOpacity>
-                <Text>This is the description of the group {groupInfo.user && groupInfo.user.first_name}.</Text>
-                {(member !== '') ? (
-                    <Pressable
-                        style={styles.button}
-                        onPress={() => leaveGroup()}>
-                        <Text style={[styles.invertText, styles.text]}>Leave Group</Text>
-                    </Pressable>) : (
-                    <Pressable
-                        style={styles.button}
-                        onPress={() => joinGroup()}>
-                        <Text style={[styles.invertText, styles.text]}>Join</Text>
-                    </Pressable>
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.navigate("Create Event", { groupId: route.params.groupId })}
+                style={styles.guideButtons}>
+                <Text style={[styles.invertText, styles.text]}>
+                    Create Event
+                </Text>
+            </TouchableOpacity>
+            <Text>This is the description of the group {groupInfo.user && groupInfo.user.first_name}.</Text>
+            <Text style={{ fontSize: 24 }}>Group Members</Text>
+            {groupMembers !== [] ? (<FlatList
+                data={groupMembers}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item.name}</Text>
+                        <Text>{item.email}</Text>
+                    </View>
                 )}
-            </View>
-        </ScrollView>
+            />) : (
+                <Text>This group does not have any members.</Text>
+            )}
+            <Text style={{ fontSize: 24 }}>Group Events</Text>
+            {groupEvents !== [] ? (<FlatList
+                data={groupEvents}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item.name}</Text>
+                    </View>
+                )}
+            />) : (
+                <Text>This group does not have any events.</Text>
+            )}
+            {(member !== '') ? (
+                <Pressable
+                    style={styles.button}
+                    onPress={() => leaveGroup()}>
+                    <Text style={[styles.invertText, styles.text]}>Leave Group</Text>
+                </Pressable>) : (
+                <Pressable
+                    style={styles.button}
+                    onPress={() => joinGroup()}>
+                    <Text style={[styles.invertText, styles.text]}>Join</Text>
+                </Pressable>
+            )}
+        </SafeAreaView>
     )
 }
--- assets/style.js
+++ assets/style.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         marginTop: Constants.statusBarHeight,
     },
